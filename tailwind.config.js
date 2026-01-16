4c05aa0b0dc244fb85f13eeb8cd4d4f93f942ac9
Aamna272 <aamnakainat19@gmail.com>
2026-01-16 14:23:13 +0500

    vertical alignment issue fixed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"], // Creates a new utility class: .font-inter
+      },
+    },
+  },
+  plugins: [],
+};
